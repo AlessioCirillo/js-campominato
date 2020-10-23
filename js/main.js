2d9cb1b65636357d93854c15b29011f58ca41247
alessio <alessio.cirillo.1995@gmail.com>
2020-10-23 15:57:48 +0200

    richiesta dati utente

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 //CREAZIONE BOMBE
 var listBomb = [];
-var size = 16;
+var size = 2;
 
 while (listBomb.length < size){
 
@@ -12,7 +12,20 @@ while (listBomb.length < size){
 }
 console.log(listBomb);
 
-
 function genRandom(max, min) {
     return Math.floor(Math.random() * (max - min + 1) ) + min;  
 }
+
+
+//RICHIESTA DATI UTENTE
+var listUtente = [];
+var sizeUtente = 6;
+
+while ( listUtente.length < sizeUtente){
+    var numeroUtente = parseInt ( prompt ('inserire un numero compreso tra 1 e 100') );
+    
+    if (listUtente.includes(numeroUtente) === false){
+         listUtente.push(numeroUtente);
+  }
+}
+console.log(listUtente);
